cf2e6955c89ab981c624b5d87eed0c7f3931d5dd
lauracurley <info@lauracurley.com>
2016-03-19 11:12:25 -0700

    Implement searchYouTube method to connect to the YouTube API

--- client/lib/searchYouTube.js
+++ client/lib/searchYouTube.js
@@ -1,5 +1,16 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: 'www.googleapis.com/youtube/v3/search?key=' + options[key] + '&maxResults=' + options[maxResults] + '&q=' + options[q] + '&videoEmbeddable=true&type=video&part=snippet', // plus some variables for our search params
+    type: 'GET',
+    data: JSON.stringify(),
+    success: function(returnedData) {
+      callback(returnedData);
+      console.log(returnedData);
+    },
+    error: function(e) {
+      console.error('searchYouTube get request didnt go through', e);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
